96e9348444b19658bf229fd274a632b942af6b68
Antonin <antochevolleau@gmail.com>
2025-04-29 14:01:00 +0200

    feat: add arrow's event listener

--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,34 @@
 const slides = [
-	{
-		"image":"slide1.jpg",
-		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
-	},
-	{
-		"image":"slide2.jpg",
-		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
-	},
-	{
-		"image":"slide3.jpg",
-		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
-	},
-	{
-		"image":"slide4.png",
-		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
-	}
-]
+  {
+    image: "slide1.jpg",
+    tagLine: "Impressions tous formats <span>en boutique et en ligne</span>",
+  },
+  {
+    image: "slide2.jpg",
+    tagLine:
+      "Tirages haute définition grand format <span>pour vos bureaux et events</span>",
+  },
+  {
+    image: "slide3.jpg",
+    tagLine: "Grand choix de couleurs <span>de CMJN aux pantones</span>",
+  },
+  {
+    image: "slide4.png",
+    tagLine: "Autocollants <span>avec découpe laser sur mesure</span>",
+  },
+];
+
+// Flèches de navigation
+
+// Récupérer les flèches dans le DOM
+const leftArrow = document.querySelector(".arrow_left");
+const rightArrow = document.querySelector(".arrow_right");
+
+// Ajout d'event listener
+leftArrow.addEventListener("click", (e) => {
+  console.log("Clic sur la flèche de gauche");
+});
+
+rightArrow.addEventListener("click", (e) => {
+  console.log("Clic sur la flèche de droite");
+});
